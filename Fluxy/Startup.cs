ca9744b99a57d79b7cb7aea7def7618709a4d468
Rajamanickam <rajamanickam.pr@gmail.com>
2017-08-23 18:16:12 +0530

    Initial Commit

---
+++ Fluxy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fluxy.Startup))]
+namespace Fluxy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
