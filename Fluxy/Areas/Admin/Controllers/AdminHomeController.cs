c41d4ce0740d50825a4c3064d4c58b7284f80b49
Rajamanickam <rajamanickam.pr@gmail.com>
2017-09-12 15:38:48 +0530

    Dashboard completed

--- Fluxy/Areas/Admin/Controllers/AdminHomeController.cs
+++ Fluxy/Areas/Admin/Controllers/AdminHomeController.cs
@@ -2,21 +2,47 @@
 using System.Web.Mvc;
 using AutoMapper;
 using Fluxy.Services.Logging;
+using Microsoft.AspNet.Identity;
+using Fluxy.Data;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
+using Fluxy.Areas.Admin.Models;
+using Fluxy.Services.Video;
 
 namespace Fluxy.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminHomeController : BaseController
     {
-        public AdminHomeController(ILogService logService, IMapper mapper)
+        private readonly FluxyContext _context;
+        private readonly IMapper _mapper;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IVideoAttributesService _videoAttributesService;
+
+        public AdminHomeController(ILogService logService, IVideoAttributesService videoAttributesService, IMapper mapper)
             : base(logService, mapper)
         {
+            _context = new FluxyContext();
+            _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            _videoAttributesService = videoAttributesService;
+            _mapper = mapper;
         }
 
         // GET: Admin/AdminHome
         public ActionResult Index()
         {
-            return View();
+            var totalRegisteredUsers = _userManager.Users.Count();
+            var AdultVideosCount = _videoAttributesService.GetList(i => i.IsAdultContent).Count();
+            var PublicVideosCount = _videoAttributesService.GetList(i => i.IsPublicVideo).Count();
+            var PrivateVideosCount = _videoAttributesService.GetList(i => !i.IsPublicVideo).Count();
+            var Admin = new AdminViewModel
+            {
+                TotalRegisteredUsers = totalRegisteredUsers,
+                AdultVideosCount = AdultVideosCount,
+                PublicVideosCount = PublicVideosCount,
+                PrivateVideosCount= PrivateVideosCount
+            };
+            return View(Admin);
         }
     }
 }
---
+++ Fluxy/Areas/Admin/Models/AdminViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fluxy.Areas.Admin.Models
+{
+    public class AdminViewModel
+    {
+        public int TotalRegisteredUsers { get; set; }
+        public int AdultVideosCount { get; set; }
+        public int PrivateVideosCount { get; set; }
+        public int PublicVideosCount { get; set; }
+
+    }
+}
